cde38bc487f55e16cdaf79b4162410e5e054dcb5
keastmin <kemin1910@gmail.com>
2025-07-10 08:43:35 +0900

    적 AI를 위한 씬 생성

---
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour, IDamageable
+{
+
+
+    public virtual void TakeDamage(int damage, Vector3 hitPoint)
+    {
+        Debug.Log("맞음");
+    }
+}
